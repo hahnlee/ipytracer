8215a9f09d7d9619143a14d18f6f6ce26f423e55
sn0wle0pard <hanlee@unist.ac.kr>
2016-12-10 19:06:31 +0900

    support readthedoc

--- docs/conf.py
+++ docs/conf.py
@@ -77,7 +77,8 @@ release = VERSION
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
 language = None
-
+locale_dirs = ['locale/']
+gettext_compact = False
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
 #
@@ -129,7 +130,15 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed from
+# docs.readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+# only import and set the theme if we're building docs locally
+if not on_rtd:
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
