0b8ccbbfed94be80bf7e1aad890b2392922f0be9
sn0wle0pard <hanlee@unist.ac.kr>
2016-12-31 21:38:38 +0900

    crate Abstract base Tracer class

--- tracer/tracer.py
+++ tracer/tracer.py
@@ -19,8 +19,127 @@ from uuid import uuid4
 from .model import ListElement
 
 
+class Tracer(DOMWidget):
+    """Abstract base class for Tracers.
+    Tracer with specific codes are specializations of this class.
+    """
+
+    data = List().tag(sync=True)
+
+    notified = Int(-1).tag(sync=True)
+    selected = Int(-1).tag(sync=True)
+    visited = Int(-1).tag(sync=True)
+
+    def __init__(self, data, delay=0.25, **kwargs):
+        super(Tracer, self).__init__(**kwargs)
+        self.data = data
+        self._data = data
+        self.delay = delay
+        self._id = uuid4()
+
+    def __add__(self, other):
+        self._data += other
+        self.data = list(self._data)  # FIXME support custom notify trait
+        time.sleep(self.delay)
+
+    def __delitem__(self, key):
+        del self._data[key]
+        self.data = list(self._data)
+        time.sleep(self.delay)
+
+    def __getitem__(self, key):
+        self.visited = key
+        time.sleep(self.delay)
+        return self.data[key]
+
+    def __len__(self):
+        return len(self.data)
+
+    def __setitem__(self, key, value):
+        self._data[key] = value
+        self.data = list(self._data)
+        self.visited = key
+        time.sleep(self.delay)
+
+    """
+    Python List data type method
+    """
+
+    def append(self, value):
+        self._data.append(value)
+        self.data = list(self._data)
+
+    def extend(self, L):
+        self._data.extend(L)
+        self.data = list(self._data)
+
+    def insert(self, i, x):
+        self._data.insert(i, x)
+        self.data = list(self._data)
+
+    def remove(self, x):
+        self._data.remove(x)
+        self.data = list(self._data)
+
+    def pop(self, i):
+        self._data.pop(i)
+        self.data = list(self._data)
+
+    def index(self, x):
+        return self.data.index(x)
+
+    def count(self, x):
+        return self.data.count(x)
+
+    def sort(self, key=None, reverse=False):
+        self._data.sort(key=key, reverse=reverse)
+        self.data = list(self._data)
+
+    def reverse(self):
+        self._data.reverse()
+        self.data = list(self._data)
+
+
+class ChartTracer(DOMWidget):
+
+    _view_name = Unicode('ChartTracerView').tag(sync=True)
+    _model_name = Unicode('ChartTracerModel').tag(sync=True)
+    _view_module = Unicode('tracer').tag(sync=True)
+    _model_module = Unicode('tracer').tag(sync=True)
+
+    data = List().tag(sync=True)
+    labels = List().tag(sync=True)
+    selected = Int(-1).tag(sync=True)
+    set = Int(-1).tag(sync=True)
+
+    @default('layout')
+    def _default_layout(self):
+        return Layout(height='300px')
+
+    def __init__(self, data, delay=0.25, **kwargs):
+        super(ChartTracer, self).__init__(**kwargs)
+        self.data = data
+        self._data = data
+        self.labels = [i for i in range(len(data))]
+        self.delay = delay
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, key):
+        self.selected = key
+        time.sleep(self.delay)
+        return self.data[key]
+
+    def __setitem__(self, key, value):
+        self._data[key] = value
+        self.data = self._data[:]
+        self.set = key
+        time.sleep(self.delay)
+
+
 class List1DTracer(DOMWidget):
-    """basic Tracer object it show list as table"""
+
     def __init__(self, data, delay=0.25, **kwargs):
         super(List1DTracer, self).__init__(**kwargs)
         self.data = data
@@ -155,44 +274,6 @@ class List1DTracer(DOMWidget):
 
     def show(self):
         self._update_html()
-
-
-class ChartTracer(DOMWidget):
-
-    _view_name = Unicode('ChartTracerView').tag(sync=True)
-    _model_name = Unicode('ChartTracerModel').tag(sync=True)
-    _view_module = Unicode('tracer').tag(sync=True)
-    _model_module = Unicode('tracer').tag(sync=True)
-
-    data = List().tag(sync=True)
-    labels = List().tag(sync=True)
-    selected = Int(-1).tag(sync=True)
-    set = Int(-1).tag(sync=True)
-
-    @default('layout')
-    def _default_layout(self):
-        return Layout(height='300px')
-
-    def __init__(self, data, delay=0.25, **kwargs):
-        super(ChartTracer, self).__init__(**kwargs)
-        self.data = data
-        self._data = data
-        self.labels = [i for i in range(len(data))]
-        self.delay = delay
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, key):
-        self.selected = key
-        time.sleep(self.delay)
-        return self.data[key]
-
-    def __setitem__(self, key, value):
-        self._data[key] = value
-        self.data = self._data[:]
-        self.set = key
-        time.sleep(self.delay)
 
 
 class List2DTracer(object):
