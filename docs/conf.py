f7c75a60f279f4825d97cbec5b039e9d36bafb39
sn0wle0pard <hanlee@unist.ac.kr>
2017-01-17 19:48:23 +0900

    fix docs name

--- docs/conf.py
+++ docs/conf.py
@@ -20,10 +20,7 @@
 import os
 import sys
 # [read the docs](read-the-docs.readthedocs.org/en/latest/faq.html)
-try:
-    from unittest.mock import MagicMock
-except:
-    from mock import Mock as MagicMock
+from unittest.mock import MagicMock
 
 
 class Mock(MagicMock):
