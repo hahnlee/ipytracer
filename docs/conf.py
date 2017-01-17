795ab0614dbdc9e05713390cc1e0a0aca548ac07
sn0wle0pard <hanlee@unist.ac.kr>
2017-01-17 20:29:54 +0900

    fix conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ from unittest.mock import MagicMock
 class Mock(MagicMock):
     @classmethod
     def __getattr__(cls, name):
-        return Mock()
+        return MagicMock()
 
 MOCK_MODULES = ['jinja2', 'IPython', 'ipywidgets']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
