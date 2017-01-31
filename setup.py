7ea04712dd823e6e44b9b5ccbd16b1e690a1cac9
sn0wle0pard <hanlee@unist.ac.kr>
2017-01-31 20:02:54 +0900

    fix setup.py for windows install

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
+# -*- coding: UTF-8 -*-
+# Copyright (c) Han Lee.
 # This setup.py is came from widget-cookiecutter
 # https://github.com/jupyter/widget-cookiecutter/blob/master/{{cookiecutter.github_project_name}}/setup.py
-# Fix package name
+# Distributed under the terms of the Modified BSD License.
 
 from __future__ import print_function
 from setuptools import setup, find_packages, Command
@@ -84,7 +86,7 @@ class NPM(Command):
 
     def has_npm(self):
         try:
-            check_call(['npm', '--version'])
+            check_call(['npm', '--version'], shell=True)
             return True
         except:
             return False
@@ -104,7 +106,7 @@ class NPM(Command):
 
         if self.should_run_npm_install():
             log.info("Installing build dependencies with npm.  This may take a while...")
-            check_call(['npm', 'install'], cwd=node_root, stdout=sys.stdout, stderr=sys.stderr)
+            check_call(['npm', 'install'], cwd=node_root, stdout=sys.stdout, stderr=sys.stderr, shell=True)
             os.utime(self.node_modules, None)
 
         for t in self.targets:
