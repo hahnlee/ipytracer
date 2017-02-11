c9005592cd165bc427c91879e82620ed1c68d571
sn0wle0pard <hanlee@unist.ac.kr>
2017-02-11 21:27:05 +0900

    1.2.0

--- ipytracer/_version.py
+++ ipytracer/_version.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
 # Copyright (c) Han Lee.
 # Distributed under the terms of the Modified BSD License.
-version_info = (0, 1, 1, 'post1')
+version_info = (0, 2, 0)
 __version__ = '.'.join(map(str, version_info))
